39453688f56dfaef9ffabcd12039f4efd288858a
Slopy <michamarzec@gmail.com>
2015-05-23 14:09:15 +0200

    scaffold for matches

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :matches
   resources :users
   devise_for :users, path: "auth",
   path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret',
