3accef44f942a7263094db39471906772435b202
Slopy <michamarzec@gmail.com>
2015-05-23 12:21:33 +0200

    devise User views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :users
+
+  root to: 'users#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
