e8869de75340ca418871613b32311ff62e5c4512
hyfsterion <hyfsterion@gmail.com>
2015-05-23 13:08:36 +0200

    Relations between models: Team and Match

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,6 @@
 class Match < ActiveRecord::Base
+  belongs_to :team_a, class_name: "Team"
+  belongs_to :team_b, class_name: "Team"
+
+  scope :with_team, ->(team_id) { where("team_a_id=? OR team_b_id=?", team_id, team_id) }
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,5 @@
 class Team < ActiveRecord::Base
+  def matches 
+    Match.with_team(self.id)
+  end
 end
