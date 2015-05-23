85b3f6760ba64fa99f9c758a7aaf3386ed2bc012
Slopy <michamarzec@gmail.com>
2015-05-23 13:50:23 +0200

    routes for devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
 
+  resources :users
+  devise_for :users, path: "auth",
+  path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret',
+    confirmation: 'verification', unlock: 'unblock', registration: 'register',
+     sign_up: 'cmon_let_me_in' }
   root to: 'users#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
