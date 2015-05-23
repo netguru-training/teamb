4edc4c1af3a9921b49736856adbf6baf63ce5687
hyfsterion <hyfsterion@gmail.com>
2015-05-23 16:55:56 +0200

    Teams actions

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,18 @@
 class TeamsController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: [:new, :create, :delete]
-  before_action :authenticate_admin!, only: [:new, :create, :delete]
+  skip_before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+  before_action :authenticate_admin!, only: [:new, :edit, :create, :update, :destroy]
 
-  expose(:team) { Team.new }
+  expose(:teams)
+  expose(:team)
+
+  def index
+  end
 
   def new
+  end
+
+  def edit
 
   end
 
@@ -17,6 +24,27 @@ class TeamsController < ApplicationController
     else
       render 'new', notice: "Team could not be created."
     end
+  end
+
+  def update
+    notice
+    binding.pry
+    if team.update(team_params)
+      notice = "Team updated successfully."
+    else
+      notice = "Team could not be updated."
+    end
+    render action: "index", notice: notice
+  end
+
+  def destroy
+    notice
+    if team.destroy
+      notice = "Team deleted successfully."
+    else
+      notice = "Team could not be deleted."
+    end
+    render action: "index", notice: notice
   end
 
   private
