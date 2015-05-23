f40708547f3f4eec8dc3b695ddaa5018bb052916
hyfsterion <hyfsterion@gmail.com>
2015-05-23 16:26:23 +0200

    Logout link - display only once

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523100531) do
+ActiveRecord::Schema.define(version: 20150523130548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email"
+    t.string   "encrypted_password"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
 
   create_table "matches", force: :cascade do |t|
     t.integer  "team_a_id"
