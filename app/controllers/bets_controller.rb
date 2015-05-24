f133e0af3c84fbb35ba4903a230f00a28247a80d
Slopy <michamarzec@gmail.com>
2015-05-24 15:25:12 +0200

    remove pry

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -7,7 +7,6 @@ class BetsController < ApplicationController
   expose(:match)
 
   def index
-    binding.pry
     bets = params[:match_id] ? Bet.where(match_id: params[:match_id]) : current_user.try(bets)
     bets = Bet.all if current_admin
   end
